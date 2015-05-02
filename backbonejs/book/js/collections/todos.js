db7225a0c3b0a1806a3b753f4c8a05b0063f6f8b
Hector Martinez III <hmartineziii@gmail.com>
2015-05-01 21:34:22 -0700

    fixed book example and added css

--- backbonejs/book/js/collections/todos.js
+++ backbonejs/book/js/collections/todos.js
@@ -1,72 +1,31 @@
-/*global Backbone */
 var app = app || {};
 
-(function () {
-	'use strict';
+var TodoList = Backbone.Collection.extend({
+	model: app.Todo,
 
-	// Todo Collection
-	// ---------------
+	localStorage: new Store('todos-backbone'),
 
-	// The collection of todos is backed by *localStorage* instead of a remote
-	// server.
-	var Todos = Backbone.Collection.extend({
-		// Reference to this collection's model.
-		model: app.Todo,
+	completed: function () {
+		return this.filter(function(todo){
+			return todo.get('completed');
+		})
+	},
 
-		// Save all of the todo items under the `"todos"` namespace.
-		localStorage: new Backbone.LocalStorage('todos-backbone'),
+	remaining: function () {
+		return this.without.apply( this, this.completed() );
+	},
 
-		// Filter down the list of all todo items that are finished.
-		completed: function () {
-			return this.where({completed: true});
-		},
+	nextOrder: function() {
+		if (!this.length) {
+			return 1;
+		}
+		return this.last().get('order') + 1
+	},
 
-		// Filter down the list to only todo items that are still not finished.
-		remaining: function () {
-			return this.where({completed: false});
-		},
+	comparator: function (todo){
+		return todo.get('order');
+	}
 
-		// We keep the Todos in sequential order, despite being saved by unordered
-		// GUID in the database. This generates the next order number for new items.
-		nextOrder: function () {
-			return this.length ? this.last().get('order') + 1 : 1;
-		},
+});
 
-		// Todos are sorted by their original insertion order.
-		comparator: 'order'
-	});
-
-	// Create our global collection of **Todos**.
-	app.todos = new Todos();
-})();
-// var app = app || {};
-
-// var TodoList = Backbone.Collection.extend({
-// 	model: app.todo,
-
-// 	localStorage: new Backbone.LocalStorage('todos-backbone'),
-
-// 	completed: function () {
-// 		return this.filter(function(todo){
-// 			return todo.get('completed');
-// 		})
-// 	},
-
-// 	remaining: function () {
-// 		return this.without.apply( this, this.completed() );
-// 	},
-
-// 	nextOrder: function() {
-// 		if (!this.length) {
-// 			return 1;
-// 		}
-// 		return this.last().get('order') + 1
-// 	},
-
-// 	comparator: function (todo){
-// 		return todo.get('order');
-// 	}
-
-// });
-
-// app.Todos = new TodoList();
+app.Todos = new TodoList();
